558f9f4fdef990e09e4afccdb552ad4a835c1cb7
Travis Wimer <notanactualemailaddress@traviswimer.com>
2018-07-26 11:49:30 -0700

    Adds default config and required props.

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ module.exports = {
 	name: "StaticCloudfront",
 	resources: {
 		CloudfrontDistribution: require('./resources/cloudfrontDistribution')
-	}
+	},
+	default_config: {
+		use_https: true
+	},
+	required_props: [
+		"domain",
+		"region",
+		"default_root_object",
+		"certificate_arn",
+		"origins"
+	]
 };
